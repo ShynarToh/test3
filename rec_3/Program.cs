7c3dbf3d40d85bdddfa3e306b0df3f9eed7cce54
Shynar <shurik_aaa@mail.ru>
2023-12-14 13:25:51 +0600

    3task

---
+++ rec_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте произвольный массив. Выведете 
+// его элементы, начиная с конца. Использовать 
+// рекурсию, не использовать циклы
+
+using System;
+public class HW_3 {
+    static public void Main (string [] arg) {
+        int [] array1 = {1, 4, 7, 20, 5, 6};
+        
+        System.Console.WriteLine(ShowArray(array1, array1.Length-1));
+
+    }
+    static string ShowArray(int [] array, int index)
+ {
+        if ( index < 0 )
+            return "";
+            return array[index] +" "+ ShowArray(array, index-1);
+       
+ }
+    
+
+    }
