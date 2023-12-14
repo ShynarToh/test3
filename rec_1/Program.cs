dbdef34cb9615165c3d093247a7884f6936a0dbb
Shynar <shurik_aaa@mail.ru>
2023-12-14 13:18:52 +0600

    1 task1

---
+++ rec_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// : Задайте значения M и N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от M до N.
+//  Использовать рекурсию, не использовать циклы.
+using System;
+public class task2{
+    static public void Main(string [] arg){
+        System.Console.WriteLine($"введите число m:");
+        int m = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine($"введите число n:");
+        int n = Convert.ToInt32(Console.ReadLine());
+       
+        System.Console.WriteLine(ShowDigits(m, n));
+    }
+    static public string ShowDigits(int m, int n){
+       
+        if (m>n)
+        return " ";
+       return m + " " + ShowDigits(m+1, n);
+    } 
+      
+ } 
